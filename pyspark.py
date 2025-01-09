8208e5d58ab5b375ac7ccfc2391aeb2f00831fba
Keith Yemelianovskyi <yayekit@gmail.com>
2025-01-08 23:52:13 -0500

    Add PySpark imports for data processing and machine learning pipeline

--- pyspark.py
+++ pyspark.py
@@ -1,3 +1,12 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import *
+from pyspark.ml.classification import *
+from pyspark.ml.regression import *
+from pyspark.ml.evaluation import *
+
 def build_medallion_pipeline():
     # bronze: raw ingestion
     bronze_df = (spark.readStream.format("kafka")
